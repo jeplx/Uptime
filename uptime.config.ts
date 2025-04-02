900412a0e27061983190a959951ce3b0aa28cd6e
James <79770879+jeplx@users.noreply.github.com>
2025-04-02 23:20:22 +0100

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -9,9 +9,9 @@ const pageConfig = {
   // [OPTIONAL] Group your monitors
   // If not specified, all monitors will be shown in a single list
   // If specified, monitors will be grouped and ordered, not-listed monitors will be invisble (but still monitored)
-  group: {
-    "🌐 Public": ['puptree_monitor', 'bar_monitor', 'more monitor ids...'],
-  },
+  // group: {
+  //  "🌐 Public": ['puptree_monitor', 'bar_monitor', 'more monitor ids...'],
+  // },
 }
 
 const workerConfig = {
@@ -19,35 +19,15 @@ const workerConfig = {
   kvWriteCooldownMinutes: 3,
   // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
   // passwordProtection: 'username:password',
-  // Define all your monitors here
   monitors: [
-    // Example HTTP Monitor
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
       id: 'puptree_monitor',
-      // `name` is used at status page and callback message
-      name: 'Puptr.ee Site',
-      // `method` should be a valid HTTP Method
+      name: 'Puptr.ee Service',
       method: 'GET',
-      // `target` is a valid URL
       target: 'https://puptr.ee',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'Check if Puptr.ee is up and working',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
       statusPageLink: 'https://puptr.ee',
-      // [OPTIONAL] `hideLatencyChart` will hide status page latency chart if set to true
-      hideLatencyChart: false,
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      responseKeyword: 'success',
-      // [OPTIONAL] if specified, the response must NOT contains the keyword to be considered as operational.
       responseForbiddenKeyword: 'bad gateway',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      checkLocationWorkerRoute: 'https://status.puptr.ee',
     },
   ],
   notification: {
@@ -58,11 +38,11 @@ const workerConfig = {
     // if not specified, no notification will be sent
     // recipientUrl: "tgram://bottoken/ChatID",
     // [Optional] timezone used in notification messages, default to "Etc/GMT"
-    timeZone: "Europe/London",
+    // timeZone: "Europe/London",
     // [Optional] grace period in minutes before sending a notification
     // notification will be sent only if the monitor is down for N continuous checks after the initial failure
     // if not specified, notification will be sent immediately
-    gracePeriod: 5,
+    // gracePeriod: 5,
   },
   callbacks: {
     onStatusChange: async (
