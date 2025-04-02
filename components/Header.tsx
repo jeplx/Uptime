7b4265152cd588b93e71383562f645f0d0e0aecd
James <79770879+jeplx@users.noreply.github.com>
2025-04-02 23:56:49 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,9 +22,6 @@ export default function Header() {
       <Container size="md" className={classes.inner}>
         <div>
           <a href="https://puptr.ee" target="_blank">
-            <Text size="xl" span>
-              🕒
-            </Text>
             <Text
               size="xl"
               span
