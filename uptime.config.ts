e8370a95ebf9d22bfa954a042d446ee81a5d8f79
James <79770879+jeplx@users.noreply.github.com>
2025-04-02 22:31:57 +0100

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,11 +1,10 @@
 const pageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "Puptr.ee Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://puptr.ee', label: 'Puptr.ee', highlight: true },
+    { link: 'https://pyropup.co.uk', label: 'Pup Pyro' },
   ],
   // [OPTIONAL] Group your monitors
   // If not specified, all monitors will be shown in a single list
