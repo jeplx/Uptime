4606b0c4b564ae15b5304b7d2431f85954f0d9b8
James <79770879+jeplx@users.noreply.github.com>
2025-04-02 23:32:38 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,19 +73,14 @@ export default function Home({
         <Text size="xs" mt="xs" mb="xs" style={{
           textAlign: 'center'
         }}>
-          Open-source monitoring and status page powered by{' '}
-          <a href="https://github.com/lyc8503/UptimeFlare" target="_blank">
-            Uptimeflare
+          {' '}
+          <a href="https://puptr.ee" target="_blank">
+            Puptr.ee
           </a>{' '}
-          and{' '}
-          <a href="https://www.cloudflare.com/" target="_blank">
-            Cloudflare
+          Status Monitor by {' '}
+          <a href="https://pyropup.co.uk" target="_blank">
+            Pup Pyro
           </a>
-          , made with ❤ by{' '}
-          <a href="https://github.com/lyc8503" target="_blank">
-            lyc8503
-          </a>
-          .
         </Text>
       </main>
     </>
